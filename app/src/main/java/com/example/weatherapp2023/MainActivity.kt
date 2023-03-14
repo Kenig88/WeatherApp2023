8f9506085358e9612e812a590466468fb0e62ccd
Fitness 2023 <14bassdrumand88@gmail.com>
2023-03-15 00:50:42 +0300

    WeatherApp 1/4

--- app/src/main/java/com/example/weatherapp2023/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp2023/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.weatherapp2023
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp2023.databinding.ActivityMainBinding
 
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
--- app/src/main/java/com/example/weatherapp2023/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp2023/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp2023
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -12,9 +13,19 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        timer = object: CountDownTimer(2000,1000){
+            override fun onTick(p0: Long) {
+
+            }
+            override fun onFinish() {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+        }.start()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 }

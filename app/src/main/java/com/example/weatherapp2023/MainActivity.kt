f4e48de5827e2fc69d1adf143d3df183e81e4c98
Fitness 2023 <14bassdrumand88@gmail.com>
2023-03-15 00:29:15 +0300

    WeatherApp 1/4

---
+++ app/src/main/java/com/example/weatherapp2023/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp2023
+
+class MainActivity {
+
+}
--- app/src/main/java/com/example/weatherapp2023/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp2023/SplashActivity.kt
@@ -3,11 +3,18 @@ package com.example.weatherapp2023
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.PersistableBundle
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private lateinit var timer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
     }
 }
